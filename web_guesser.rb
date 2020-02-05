2c7358d569dbc1452d619be06748a7a49582fd62
unheavenlycreature <imaginaryenemy@protonmail.com>
2020-02-05 18:12:29 -0500

    Create a secret number and display it.

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
 require 'sinatra'
+require 'sinatra/reloader'
 
-get '/' do 'Hello World!' end
+secret_number = rand(101)
+
+get '/' do
+  "The SECRET NUMBER is #{secret_number}"
+end
