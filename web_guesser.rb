b4f2b6a7c4d76928b6a8e392024681b78e25051c
unheavenlycreature <imaginaryenemy@protonmail.com>
2020-02-05 18:26:33 -0500

    Finish number guessing game.

--- web_guesser.rb
+++ web_guesser.rb
@@ -6,5 +6,13 @@ require 'sinatra/reloader'
 secret_number = rand(101)
 
 get '/' do
-  erb :index, locals: {secret_number: secret_number}
+  guess = params['guess'].to_i
+  message = "Correct! The secret number is #{secret_number}"
+  if guess > secret_number
+    message = 'Too high!'
+  elsif guess < secret_number
+    message = 'Too low!'
+  end
+
+  erb :index, locals: { secret_number: secret_number, message: message }
 end
