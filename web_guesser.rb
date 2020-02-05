12ab7cca497d030d2a1d714745b3df82efe88da5
unheavenlycreature <imaginaryenemy@protonmail.com>
2020-02-05 18:45:11 -0500

    End the game after five turns and change background color based on guess.

--- web_guesser.rb
+++ web_guesser.rb
@@ -4,15 +4,33 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 secret_number = rand(101)
+remaining_guesses = 5
 
 get '/' do
+  message = ''
   guess = params['guess'].to_i
-  message = "Correct! The secret number is #{secret_number}"
-  if guess > secret_number
-    message = 'Too high!'
-  elsif guess < secret_number
-    message = 'Too low!'
+
+  if guess == secret_number
+    message = "Correct, the secret number is #{secret_number}!"
+    remaining_guesses = 5
+    secret_number = rand(101)
+    color = 'green'
+  else
+    remaining_guesses -= 1
+    if remaining_guesses.zero?
+      message = "Game over! The secret number was #{secret_number}. Guess the new number!"
+      remaining_guesses = 5
+      secret_number = rand(101)
+      color = 'red'
+    else
+      message = guess > secret_number ? 'Too high!' : 'Too low!'
+      color = (secret_number - guess).abs < 30 ? 'lightpink' : 'red'
+    end
   end
 
-  erb :index, locals: { secret_number: secret_number, message: message }
+  erb :index, locals: {
+    secret_number: secret_number,
+    message: message,
+    color: color
+  }
 end
